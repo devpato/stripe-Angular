6a1f15fefb8d5e343ee527e4f65c78ec9af6884b
pevargasg <wearetamo@gmail.com>
2018-10-17 22:02:32 -0700

    added component and service to .module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { PaymentService } from "./payments/payment.service";
+import { MakePaymentComponent } from "./payments/make-payment/make-payment.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, MakePaymentComponent],
+  imports: [BrowserModule],
+  providers: [PaymentService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
