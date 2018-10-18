d456a186e709ae848988966e84167c345345c833
pevargasg <wearetamo@gmail.com>
2018-10-17 22:00:13 -0700

    created payment component

--- src/app/payments/payment/payment.module.ts
+++ src/app/payments/payment/payment.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MakePaymentComponent } from '../make-payment/make-payment.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [MakePaymentComponent]
 })
 export class PaymentModule { }
