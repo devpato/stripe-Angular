bce674edc6fb89dbc76189286aadb09c3d72cc46
pevargasg <wearetamo@gmail.com>
2018-10-17 21:59:16 -0700

    added stripe

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  stripekey: "pk_test_lW1MY7AU5CKwr5EH6W004v9f"
 };
 
 /*
